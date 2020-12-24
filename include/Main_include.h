0e0451028e82b548a376938e133479210c4a87c5
Dejvid Bushi <dejvid.bushi@gmail.com>
2020-12-24 14:32:13 +0200

    include files in Main_include.h

--- include/Main_include.h
+++ include/Main_include.h
@@ -23,5 +23,19 @@ using namespace std;
 #include "Market.h"
 #include "Common.h"
 
+#include "Living/Living.h"
 
+#include "Living/Hero/Hero.h"
+#include "Living/Hero/Warrior.h"
+#include "Living/Hero/Sorcerer.h"
+#include "Living/Hero/Paladin.h"
 
+#include "Living/Monster/Monster.h"
+#include "Living/Monster/Dragon.h"
+#include "Living/Monster/Exoskeleton.h"
+#include "Living/Monster/Spirit.h"
+
+#include "Spell/Spell.h"
+#include "Spell/FireSpell.h"
+#include "Spell/IceSpell.h"
+#include "Spell/LightingSpell.h"
