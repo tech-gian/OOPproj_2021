728241d8d02cd3c0e6f3d68a00ba5d6fbb80b75a
Dejvid Bushi <dejvid.bushi@gmail.com>
2020-12-24 14:21:54 +0200

    game constants

---
+++ include/constants.h
@@ -0,0 +1,36 @@
+// Living
+const int INIT_HEALTH_POWER = 100;
+
+// Hero
+const int INIT_MAGIC_POWER = 100;
+const int INIT_STRENGTH = 100;
+const int INIT_DEXTERITY = 100;
+const int INIT_AGILITY = 100;
+const int INIT_MONEY = 1000;
+
+// Monster
+const int MIN_DAMAGE = 10;
+const int MAX_DAMAGE = 20;
+const int INIT_DEFENSE = 100;
+const float POSSIBILITY_OF_AVOIDANCE = 0.5;
+
+// Spell
+const int FS_PRICE = 10;
+const int IS_PRICE = 9;
+const int LS_PRICE = 8;
+
+const int FS_MIN_LEVEL = 3;
+const int IS_MIN_LEVEL = 2;
+const int LS_MIN_LEVEL = 1;
+
+const int FS_MIN_DAMAGE = 15;
+const int IS_MIN_DAMAGE = 10;
+const int LS_MIN_DAMAGE = 5;
+
+const int FS_MAX_DAMAGE = 25;
+const int IS_MAX_DAMAGE = 18;
+const int LS_MAX_DAMAGE = 12;
+
+const int FS_MAGIC_POWER_REQUIRED = 20;
+const int IS_MAGIC_POWER_REQUIRED = 10;
+const int LS_MAGIC_POWER_REQUIRED = 5;
