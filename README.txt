472abfeab123d0a0f702ea749b49c9d4d167cb14
tech-gian <sdi1900059@di.uoa.gr>
2021-01-02 14:21:06 +0200

    Fix, available Items/Spells

--- README.txt
+++ README.txt
@@ -7,6 +7,3 @@ TODO: Check, in a call, main_game
 TODO: decide where functions will be (Hero or Grid)
 
 Γενικές παρατηρήσεις:
-- Τα items & τα Spells δεν φαινεται να παίρνουν όνομα. (συνδεεται με τηυν 3 -)
-- If size.checkInventory == 0, don't display "Available items/spells"
-- CHECK ifstream τι παει λαθος
--- modules/Grid/Square/Market.cc
+++ modules/Grid/Square/Market.cc
@@ -150,28 +150,34 @@ void Market::displayAvailable() {
 
     // Display every Item and Spell on player
     cout << "Depending on your bag, you can sell:" << endl;
-    cout << "Available Items:" << endl;
 
     for (int i=0 ; i<3 ; ++i) {
         displayStats(heroes[i]);
 
         list<Item*> items = heroes[i]->getInventory();
-        list<Item*>::iterator it;
-        for (it=items.begin() ; it!=items.end() ; ++it) {
-            (*it)->print();
+        if (items.size() == 0) cout << "This hero doesn't have Items" << endl;
+        else {
+            cout << "Available Items:" << endl;
+
+            list<Item*>::iterator it;
+            for (it=items.begin() ; it!=items.end() ; ++it) {
+                (*it)->print();
+            }
         }
     }
-
-    cout << "Available Spells:" << endl;
 
     for (int i=0 ; i<3 ; ++i) {
         displayStats(heroes[i]);
 
         list<Spell*> spells = heroes[i]->getSpells();
-        list<Spell*>::iterator it;
-        for (it=spells.begin() ; it!=spells.end() ; ++it) {
-            (*it)->print();
+        if (spells.size() == 0) cout << "This hero doesn't have Spells" << endl;
+        else {
+            cout << "Available Spells:" << endl;
+        
+            list<Spell*>::iterator it;
+            for (it=spells.begin() ; it!=spells.end() ; ++it) {
+                (*it)->print();
+            }
         }
     }
 }
-
